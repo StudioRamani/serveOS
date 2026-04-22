4a4961dc98738e300a316ba995031fc45b85224e
Jerrard Peace <jerrardpeace@PJP-MAX.local>
2026-04-22 14:35:50 +0200

    Fix API base URL

--- serve-app/client/src/utils/api.js
+++ serve-app/client/src/utils/api.js
@@ -1,11 +1,5 @@
-import axios from 'axios';
-
-const api = axios.create({ baseURL: '/api' });
-
-api.interceptors.request.use(cfg => {
-  const token = localStorage.getItem('token');
-  if (token) cfg.headers.Authorization = `Bearer ${token}`;
-  return cfg;
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 api.interceptors.response.use(
